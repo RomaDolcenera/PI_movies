3cf0be563f1ba07f8f234e735773d5a5bacbd371
Rocio Marquez <rocio.m@hotmail.it>
2024-07-28 00:50:54 -0300

    main.py/arreglos

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from functions import buscames
 
-dfm = pd.read_csv("/Datasets_limpios/movies_dataset_clean.csv.csv",sep = ',', encoding="UTF-8", low_memory=False)
-dfc = pd.read_csv("/Datasets_limpios.csv/credits_dataset_clean.csv")
+
+dfm = pd.read_csv("./Datasets_limpios/movies_dataset_clean.csv")
+dfc = pd.read_csv("./Datasets_limpios/credits_dataset_clean.csv")
+from functions import buscames
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"mensaje": "Bienvenido a la API de películas"}
-
 
 #Se ingresa un mes en idioma Español. Debe devolver la cantidad de películas que fueron estrenadas en el mes consultad
 # en la totalidad del dataset.
@@ -24,7 +24,7 @@ def read_item(mes: str):
     
     result_filter = dfm[dfm['release_month'] == mes_numero]
     cantidad_peliculas = len(result_filter)
-    mensaje = f'{cantidad_peliculas} fueron estrenadas en el mes de {mes}'
+    mensaje = f'Un total de {cantidad_peliculas} peliculas fueron estrenadas en el mes de {mes}'
     return {'mensaje': mensaje}
 
 #Se ingresa un día en idioma Español. Debe devolver la cantidad de películas que fueron estrenadas en día consultado en
@@ -41,25 +41,25 @@ def read_item(dia: str):
         mensaje = f'{cantidad_peliculas} peliculas fueron estrenadas en el dia {dia}'
         return {'mensaje':mensaje}
 
-
 #Se ingresa el título de una filmación esperando como respuesta el título, el año de estreno y el score.
 @app.get('/titulo_de_la_filmacion/{title}')
 def read_item(title: str):
     title = title.lower()
+    print(f"Received title: {title}")  
     result_filter = dfm.loc[dfm['title'].str.lower() == title]
     if result_filter.empty:
-        return{'La pelicula ingresada no se encuentra en la base de datos, pruebe con otro nombre'}
+        print("No matching film found")  
+        return {'error': 'La pelicula ingresada no se encuentra en la base de datos, pruebe con otro nombre'}
     else:
         release_year = result_filter['release_year'].values[0]
         popularity = result_filter['popularity'].values[0]
         mensaje = f'La filmacion {title} fue estrenada en el año {release_year} con un score de {popularity}'
         return {'mensaje': mensaje}
-
-
+    
 #Se ingresa el título de una filmación esperando como respuesta el título, la cantidad de votos y el valor promedio de las votaciones.
 # La misma variable deberá de contar con al menos 2000 valoraciones, caso contrario, debemos contar con un mensaje avisando que no cumple
 # esta condición y que por ende, no se devuelve ningun valor.
-@app.get('/titulo_de_la_filmacion/{titulo}')
+@app.get('/titulo_filmacion/{titulo}')
 def read_item(titulo: str):
     titulo = titulo.lower()
     result_filter = dfm.loc[dfm['title'].str.lower() == titulo]
@@ -74,45 +74,44 @@ def read_item(titulo: str):
     
     titulo_original = result_filter['title'].values[0]
     anio_estreno = result_filter['release_year'].values[0]
-    mensaje = f'La película {titulo_original} fue estrenada en el año {anio_estreno}. 
-    La misma cuenta con un total de {votos} valoraciones, con un promedio de {promedio_votos}'
+    mensaje = f'La película {titulo_original} fue estrenada en el año {anio_estreno} La misma cuenta con un total de {votos} valoraciones, con un promedio de {promedio_votos}'
     
     return {'mensaje': mensaje}
-
 
 #Se ingresa el nombre de un actor que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno.
 # Además, la cantidad de películas que en las que ha participado y el promedio de retorno. La definición no deberá considerar directores.
 @app.get('/get_actor/{nombre_actor}')
 def read_item(nombre_actor: str):
-    actor = dfc[dfc['actores'] == nombre_actor]
-    total_peliculas = actor.shape[0]
-    retorno_total = actor['return'].sum()
-    promedio_return = actor['return'].mean()
-    mensaje = f'El actor {nombre_actor} participo de {total_peliculas} con un retorno de {retorno_total} y un promedio de {promedio_return:2f} por pelicula.'
-    return {'mensaje': mensaje}
-
+    actor = dfc.loc[dfc['actores'].str.lower().apply(lambda x: nombre_actor.lower() in x), 'id']
+    if actor.empty:
+        return f"Error: No se encontraron coincidencias para el actor {nombre_actor}."
+    else:
+        movies = dfm[dfm.id.isin(actor)]
+        film_count  = movies.shape[0]
+        total_gross_return  = movies['revenue'].sum()
+        average_gross_return  = total_gross_return / film_count if film_count > 0 else 0
+        mensaje = f"El actor Tom Cruise ha participado de {film_count} cantidad de filmaciones, el mismo ha conseguido un retorno de {total_gross_return} con un promedio de {average_gross_return} por filmación"
+        return {'mensaje': mensaje}
+            
 
 #Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
 # Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma.
 @app.get("/get_director/{director}")
 def read_item(director: str):
     name_director = director.lower()
-    result_filter = dfc.loc[dfc['directors'].str.lower().apply(lambda x: name_director in x)]
+    result_filter = dfc.loc[dfc['directores'].str.lower().apply(lambda x: name_director in x)]
     if result_filter.empty:
-        return {"error": "El director ingresado no se encuentro en la base de datos, pruebe con el nombre completo o ingrese otro director"}
+        return {"error": "El director ingresado no se encuentra en la base de datos, pruebe con el nombre completo o ingrese otro director"}
     else:
         id_movies = result_filter['id'].to_list()
         movies_director = dfm[dfm['id'].isin(id_movies)]
         best_movie = movies_director.loc[movies_director['popularity'].idxmax()]
         column_rename = {'title': 'Titulo', 'release_year': 'Año de lanzamiento', 'return': 'Retorno', 'budget': 'Costo', 'revenue': 'Ganancia'}
         list_movies = movies_director[['title','release_year', 'return', 'budget', 'revenue']].rename(columns=column_rename).to_dict('records')
-        return {
-            "Director": name_director,
-            "Mejor Pelicula": best_movie['title'],
+        return { "Datos del Director": name_director, "Mejor Pelicula": best_movie['title'],
             "Lista de Peliculas": list_movies
-            }
-
-
+        }
+        
 @app.get("/recomendacion/{titulo}")
 def read_item(titulo: str):
     def get_recommendations(titulo, dfm):
